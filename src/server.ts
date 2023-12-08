3af2302fc9f6961d543382e63f2789fea6bb13d0
Aleksandro <adantasm@gmail.com>
2023-12-08 19:11:18 -0300

    Configurações ESlint

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
-const http = require('http');
-const server = http.createServer((req, res) =>{
-    res.setHeader('Content-Type', 'text/plain');
-})
-server.listen(3000, ()=>{
-    console.log('Servidor em execução em http://localhost:3000');
-});
+// const http = require('http');
+// const server = http.createServer((req, res) =>{
+//     res.setHeader('Content-Type', 'text/plain');
+// })
+// server.listen(3000, ()=>{
+//     console.log('Servidor em execução em http://localhost:3000');
+// });
+
