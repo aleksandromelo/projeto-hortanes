2263ca9bb49ff7f73fc4a12360a6788c0736ce36
Aleksandro <adantasm@gmail.com>
2023-12-12 16:35:32 -0300

    Configurações do Express

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,16 @@
-// const http = require('http');
-// const server = http.createServer((req, res) =>{
-//     res.setHeader('Content-Type', 'text/plain');
-// })
-// server.listen(3000, ()=>{
-//     console.log('Servidor em execução em http://localhost:3000');
-// });
+import express from "express";
 
+const app = express();
+const PORT = 3000;
+
+app.get("/clientes", (req, res) => {
+    res.send("Clientes");
+});
+
+app.get("/fornecedores", (req, res) => {
+    res.send("Fornecedores");
+});
+
+app.listen(PORT, ()=>{
+    console.log("Servidor em execução em http://localhost:3000");
+});
