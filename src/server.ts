e4e4a4859c2478362e912ac8c06294132651109c
Aleksandro <adantasm@gmail.com>
2023-12-27 11:14:43 -0300

    Tratamento de endereços duplicados ao tentar criar novo cliente.

--- src/server.ts
+++ src/server.ts
@@ -9,40 +9,97 @@ app.use(express.json());
 
 app.post("/customer", async (req, res) => {
     const { name, cnpj, email, status, customer_adress, customer_contact } = req.body;
-    await prisma.customer.create({
-        data: {
-            name,
-            cnpj,
-            email,
-            status,
-            customer_adress:{
-                create: customer_adress,
+
+    try {
+        // Verifica se o endereço já existe no banco
+        const existingAdress = await prisma.customer_adress.findUnique({
+            where: {
+                id: customer_adress.customer_id
+            }
+        });
+
+        // Se o endereço já existe, retorna um erro
+        if (existingAdress) {
+            return res.status(500).send({ message: "Failed to register customer, duplicate address." });
+        }
+
+        // Se o endereço não existe, continua com a criação do cliente
+        await prisma.customer.create({
+            data: {
+                name,
+                cnpj,
+                email,
+                status,
+                customer_adress: {
+                    create: customer_adress,
+                },
+                customer_contact: {
+                    create: customer_contact,
+                },
             },
-            customer_contact:{
-                create: customer_contact,
+            include: {
+                customer_adress: true,
+                customer_contact: true,
             },
-        },
-        include:{
-            customer_adress: true,
-            customer_contact: true,
-        },
-    });
-    res.status(201);
+        });
+
+        res.status(201).send({ message: "Customer registered successfully." });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Failed to register customer." });
+    }
 });
+
+// app.post("/customer", async (req, res) => {
+//     const { name, cnpj, email, status, customer_adress, customer_contact } = req.body;
+
+//     const noAdressDuplicate = await prisma.customer_adress.findUnique({
+//         where:{
+//             customer_id: customer_adress.customer_id
+//         },
+
+//         if(noAdressDuplicate){
+//             return res.status(500).send({message: "Failed to register customer, duplicate adress."});
+//         }
+//     });
+//     try {
+//         await prisma.customer.create({
+//             data: {
+//                 name,
+//                 cnpj,
+//                 email,
+//                 status,
+//                 customer_adress:{
+//                     create: customer_adress,
+//                 },
+//                 customer_contact:{
+//                     create: customer_contact,
+//                 },
+//             },
+//             include:{
+//                 customer_adress: true,
+//                 customer_contact: true,
+//             },
+//         });
+//     } catch (error) {
+//         return res.status(500).send({message: "Failed to register customer."});
+//     }
+//     res.status(201);
+// });
 
 app.get("/customers", async (_, res) => {
     const customers = await prisma.customer.findMany();
     res.status(200).json(customers);
 });
 
-app.get("/customer/:id", async (req, res) => {
-    const index = req.params.id;
-    const result = await prisma.customer.findUnique({
-        where:{
-            id: 1,
-        }
-    });
-});
+// app.get("/customer/:id", async (req, res) => {
+//     const index = req.params.id;
+//     const result = await prisma.customer.findUnique({
+//         where:{
+//             id: 1,
+//         }
+//     });
+// });
 
 app.get("/suppliers", async (_, res) => {
     const suppliers = await prisma.supplier.findMany();
